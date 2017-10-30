fa39502fefd9583f052fbe730f26f5ea2c0468ba
Mikhail Lutskiy <mikhail@lwts.ru>
2017-10-30 22:03:42 +0300

    First commit

---
+++ CourseWorkBlood/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CourseWorkBlood
+{
+    public partial class Form1 : Form
+    {
+        /*
+         * Конструктор класса
+         */
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /*
+         * Метод нажатия на кнопку ОК
+         * Описание: Данный метод проверяет на заполнение всех полей в форме и открывает следующую форму/выводит сообщение об ошибки.
+         */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool isAnyRadioButtonChecked = false; //Переменная для проверки выбора Radio кнопки (по умолчанию false - невыбранно)
+            Int16 goods_id = 0; //Индекс выбранных продуктов - 0 невыбранно
+            foreach (RadioButton rdo in groupBox1.Controls.OfType<RadioButton>()) //Цикл по всем Radio кнопкам в GroupBox на форме
+            {
+                if (rdo.Checked) //Если Radio кнопка выбрана, то запоминаем ее и прерываем цикл, так как Radio кнопка может быть выбрана только одна
+                {
+                    goods_id = Convert.ToInt16(rdo.Tag); //Запоминаем индекс выбранной радио кнопки (индекс задается в конструкторе в аттрибуте Tag
+                    isAnyRadioButtonChecked = true; //Ставим флаг, что кнопка выбрана
+                    break; // досрочно прерываем цикл обхода
+                }
+            }
+            if (isAnyRadioButtonChecked && comboBox1.SelectedIndex != -1) // условие: если выбрана Radio кнопка и выбрана группа крови (индекс группы крови в ComboBox не должен быть равен -1), то открываем окно с формой
+            {
+                Info info = new Info(); //Создаем объект новой формы
+                info.goods_id = goods_id; //Передаем индекс выбранных продуктов в новую форму
+                info.blood_id = Convert.ToInt16(comboBox1.SelectedIndex); //Передаем выбранную группу крови в новую форму
+                this.Hide(); //Скрываем текущую форму
+                info.ShowDialog(); //Показываем новую форму
+                this.Show(); //Показываем предыдущую форму, если та новая будет закрыта
+            }
+            else // если условие не выполнилось, то выдаем ошибку
+            {
+                MessageBox.Show("Заполните все поля!"); //Отображения окна с ошибкой
+            }
+        }
+    }
+}
---
+++ CourseWorkBlood/Info.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CourseWorkBlood
+{
+    partial class Info
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(13, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(16, 52);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(524, 266);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Info
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 330);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Info";
+            this.Text = "Информация";
+            this.Shown += new System.EventHandler(this.Info_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
